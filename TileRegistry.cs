186aacc214787cdfd13568a747b3a74d2b4848bd
DenysSheremetNURE <denys.sheremet@nure.ua>
2026-02-15 21:28:38 +0100

    New class TileRegistry added for O(1) search by id time

---
+++ TileRegistry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2048;
+
+internal class TileRegistry
+{
+    private Dictionary<int, Tile> _registry;
+
+    public TileRegistry()
+    {
+        _registry = new Dictionary<int, Tile>();
+    }
+
+    public Tile? this[int idx]
+    {
+        get 
+        {
+            return _registry.GetValueOrDefault(idx);
+        }
+    }
+
+    public void Register(Tile tile)
+    {
+        _registry.Add(tile.Id, tile);
+    }
+
+    public void Unregister(int id) 
+    {
+        _registry.Remove(id); //Remove() will check existence of element no need for if
+    }
+}
