1099276a78d291ba7aa5c1cde6c6c8fcd6ad79e0
Denys-Sheremet <denys.sheremet.ua@gmail.com>
2026-02-19 01:53:13 +0100

    Refactor: established professional project structure. Tests project is now visible

---
+++ Console2048.Test/GridTests.cs
@@ -0,0 +1,96 @@
+﻿using Console2048;
+using Xunit;
+
+namespace Console2048.Test;
+
+public class GridTests
+{
+    [Fact]
+    public void ConstructorCreates_GridWith_CorrectProperties()
+    {
+        Grid grid = new Grid(width: 4, height: 4);
+
+        Assert.Equal((4, 4), (grid.Width, grid.Height));
+        Assert.Equal(0, grid.Score);
+    }
+
+    [Fact]
+    public void GetCount_ReturnsCorrect_AmountOf_TilesStored()
+    {
+        Grid grid = new Grid(4, 4);
+        grid[2, 2] = new Tile(1, 0, 0, 0, 0, false, 2);
+        grid[1, 1] = new Tile(2, 1, 1, 0, 0, false, 2);
+
+        int countOfTiles = grid.GetCount();
+
+        Assert.Equal(2, countOfTiles);
+    }
+
+    [Fact]
+    public void InsertionOf_TileWith_IndexerSync_BothField_AndList_OfTiles()
+    {
+        Grid grid = new Grid(4, 4);
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+        grid[0, 0] = tile;
+
+        Assert.NotNull(grid[0, 0]);
+        Assert.Equal(1, grid.GetCount());
+        Assert.Equal(tile, grid[0]);
+    }
+
+    [Fact]
+    public void SnapshotStores_AllImportant_Information_ToRestore()
+    {
+        Grid grid = new Grid(4, 4);
+        Tile tile1 = new Tile(1, 2, 2, 0, 0, false, 2);
+        Tile tile2 = new Tile(2, 1, 1, 0, 0, false, 2);
+        grid[2, 2] = tile1;
+        grid[1, 1] = tile2;
+
+        StateSnapshot ss = grid.CreateSnapshot();
+
+        Assert.NotNull(ss);
+        Assert.Equal((4, 4, 0), (ss.Width, ss.Height, ss.Score));
+        Assert.Equal(2, ss.TileSnapshots.Count);
+        Assert.True(ss.TileSnapshots.Where(t => t.Id == 1).Any());
+
+    }
+
+    [Fact]
+    public void GetEmptyCells_Returns_CorrectAmount_OfEmpty_Cells_InGrid()
+    {
+        Grid grid = new Grid(2, 2);
+        grid[0, 0] = new Tile(1, 0, 0, 0, 0, false, 2);
+        grid[1, 1] = new Tile(2, 1, 1, 0, 0, false, 2);
+
+        List<(int x, int y)> emptyCells = grid.GetEmptyCells();
+
+        Assert.Equal(2, emptyCells.Count);
+    }
+
+    [Fact]
+    public void TryFindTile_ReturnsTrue_IfTile_IsPresent_InGrid()
+    {
+        Grid grid = new Grid(4, 4);
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+
+        grid[0, 0] = tile;
+
+        bool isFound = grid.TryFindTile(id: 1, out Tile? result);
+
+        Assert.True(isFound);
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+    }
+
+    [Fact]
+    public void TryFindTile_Returns_NullIf_TileIsMissing()
+    {
+        Grid grid = new Grid(4, 4);
+
+        bool isFound = grid.TryFindTile(id: 1, out Tile? result);
+
+        Assert.False(isFound);
+        Assert.Null(result);
+    }
+}
---
+++ Console2048.Test/TileTests.cs
@@ -0,0 +1,109 @@
+using Console2048;
+using Xunit;
+
+namespace Console2048.Test;
+
+public class TileTests
+{
+    [Fact]
+    public void Constructor_SetData_Correctly()
+    {
+        Tile tile = new Tile(id: 1, posX: 2, posY: 3, previousX: 2, previousY: 4, isMerged: true, value: 8);
+
+        Assert.Equal(1, tile.Id);
+        Assert.Equal(2, tile.PosX);
+        Assert.Equal(3, tile.PosY);
+        Assert.Equal(2, tile.PreviousX);
+        Assert.Equal(4, tile.PreviousY);
+        Assert.True(tile.IsMerged);
+        Assert.Equal(8, tile.Value);
+    }
+
+    [Fact]
+    public void PositionCan_BeChanged_Properly()
+    {
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+        tile.SetPosition(2, 0);
+        tile.SetPosition(2, 2);
+
+        Assert.Equal((2, 2), (tile.PosX, tile.PosY));
+    }
+
+    [Fact]
+    public void PreviousCan_BeChanged_Properly()
+    {
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+        tile.SetPrevious(2, 0);
+        tile.SetPrevious(2, 2);
+
+        Assert.Equal((2, 2), (tile.PreviousX, tile.PreviousY));
+    }
+
+    [Fact]
+    public void ValueCan_BeChanged()
+    {
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+        tile.SetValue(4);
+
+        Assert.Equal(4, tile.Value);
+    }
+
+    [Fact]
+    public void IsMerged_FlagCan_BeChanged()
+    {
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+        tile.SetMerged(true);
+
+        Assert.True(tile.IsMerged);
+    }
+
+
+
+    [Fact]
+    public void ParentsShould_BeNull_WhenInit()
+    {
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+
+        Assert.Null(tile.Parents);
+    }
+
+    [Fact]
+    public void SetParents_StoresParents_IdsProvided()
+    {
+        Tile tile = new Tile(3, 0, 0, 0, 0, true, 4);
+        tile.SetParents(1, 2);
+
+        Assert.Equal((1,2), (tile.Parents.Value.Id1, tile.Parents.Value.Id2));
+    }
+
+    [Fact]
+    public void SetParents_WithNo_ArgsSets_Null()
+    {
+        Tile tile = new Tile(3, 0, 0, 0, 0, true, 4);
+        tile.SetParents(1, 2);
+        tile.SetParents();
+
+        Assert.Null(tile.Parents);
+    }
+
+    [Fact]
+    public void SyncPrevious_MakesPrevious_SameAs_Position()
+    {
+        Tile tile = new Tile(1, 2, 2, 0, 2, false, 2);
+        tile.SyncPrevious();
+
+        Assert.Equal(2, tile.PreviousX);
+    }
+
+    [Fact]
+    public void UpdatePosition_ChangesPosition_ButKeepPrevious()
+    {
+        Tile tile = new Tile(1, 0, 0, 0, 0, false, 2);
+        tile.UpdatePosition(0, 2);
+
+        Assert.Equal(2, tile.PosY);
+        Assert.Equal(0, tile.PreviousY);
+    }
+
+    
+}
