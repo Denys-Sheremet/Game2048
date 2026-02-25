55ee352497fb0ebc1763ada11182ec1a3f6630c8
Denys-Sheremet <denys.sheremet.ua@gmail.com>
2026-02-25 23:09:41 +0100

    Added Interface for history and a wrapper of a stack of state history for future updates

---
+++ Console2048/HistoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2048;
+
+internal class HistoryManager : IHistoryManager
+{
+    private readonly Stack<StateSnapshot> _stack = new();
+
+    public int Count => _stack.Count;
+    public bool IsEmpty => _stack.Count == 0;
+    public void Push(StateSnapshot state) => _stack.Push(state);
+    public StateSnapshot? Pop() => _stack.Count > 0 ? _stack.Pop() : null;
+    public void Clear() => _stack.Clear();
+}
---
+++ Console2048/IHistoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2048;
+
+internal interface IHistoryManager
+{
+    int Count { get; }
+    bool IsEmpty => Count == 0;
+    void Push(StateSnapshot state);
+    StateSnapshot? Pop();
+    void Clear();
+}
