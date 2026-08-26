0802652be39252d15d8ea0de662f0d2d1d7f0176
Denys-Sheremet <denys.sheremet.ua@gmail.com>
2026-08-26 18:40:17 +0200

    Add ProfilePageViewModel. Refactor incorrect DynamicResource PageBackground in xaml pages

---
+++ Game2048.Maui/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Game2048.Maui.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048.Maui.ViewModels;
+
+public partial class ProfilePageViewModel : BindableObject
+{
+    private readonly IProfileManager _profileManager;
+    private readonly IThemesManager _themesManager;
+
+
+
+    public ProfilePageViewModel(IProfileManager profileManager, IThemesManager themeManager)
+    {
+        _profileManager = profileManager;
+        _themesManager = themeManager;
+    }
+
+
+}
